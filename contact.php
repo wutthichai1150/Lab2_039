af1578934f9fd078df67dc161645ef6ab74d6ffc
Wutthichai <wutthichai.kh@rmutsvmail.com>
2025-01-08 09:37:58 +0700

    แก้ไขไฟล์ contact ครั้ง3

--- contact.php
+++ contact.php
@@ -17,7 +17,6 @@
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
-				<li><a href="detail.php">รายละเอียดงับ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
